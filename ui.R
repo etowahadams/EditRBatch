68c100b9d97bc236be75b98a024a93fdfb816b30
etowahadams <etowahadams@gmail.com>
2025-03-19 14:43:05 -0400

    fix: update UI description

--- ui.R
+++ ui.R
@@ -2,8 +2,8 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput("files", "Choose ab1 files", multiple=TRUE),
-      fileInput("data.key", "Choose sample file 'Sample ID	gRNA	guide sequence	Reverse Y/N
-'", multiple=TRUE)
+      fileInput("data.key", "Choose metadata file (format: 'Sample ID,	gRNA	guide sequence,	Reverse Y/N
+')", multiple=TRUE)
     ),
     mainPanel(
       tableOutput("contents")
